3dd1f7b20ee773418203d3913dfc3c913387670d
hena14758 <hena14758@hundsun.com>
2019-08-01 19:40:12 +0800

    新增图标模块

---
+++ src/pages/Home/components/Icons.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="icons-frame">
+    <div class="icon">
+      <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"/>
+      <p>热门景点</p>
+    </div>
+    <div class="icon">
+    </div>
+    <div class="icon">
+    </div>
+
+  </div>
+</template>
+<script>
+export default {
+  nama: 'HomeIcons',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .icons-frame
+    height: 0
+    padding-bottom: 50%
+    background-color : blue
+    .icon
+      width:25%
+      padding-bottom: 25%
+      background-color: green
+      float: left
+      overflow: hidden
+      height: 0
+      .icon-img
+        width: 100%
+        padding-bottom :70%
+
+</style>
